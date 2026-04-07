e6e4d9ef6e2f8cba6ffd1dc8a42abf1a3e2cc5a1
almaudud <almadudiakunngoding@gmail.com>
2026-04-07 18:32:28 +0700

    menambahkan deklarasi variasi struct data nama, data alamat, dan data umur

--- struct.cpp
+++ struct.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 struct orang{
-    string name;
+    string nama;
     string alamat;
+    string umur;
     int user;
+};
+
+int main(){
+    //Deklarasi variasi struct
+    orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data Tampil" << endl;
+    cout << "Nama : "  << mhs.nama  << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "umur : " << mhs.umur << endl;
 }
