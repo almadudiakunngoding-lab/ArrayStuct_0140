78460fa6cf39cadcc9d24dba7595063c84f4d607
almaudud <almadudiakunngoding@gmail.com>
2026-04-07 19:01:10 +0700

    menambahkan  deklarasi variable struct

---
+++ structarray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    orang mhs[3];
+
+    
