5523be371ba92a4560d99bd5bd46620f05583e57
almaudud <almadudiakunngoding@gmail.com>
2026-04-07 17:42:33 +0700

    menambahkan cara akses dan mengisi array

--- Array.cpp
+++ Array.cpp
@@ -4,3 +4,10 @@ using namespace std;
 int main(){
     int dataku[5] = {10, 20, 30, 40, 50};
     int datamu[5];
+
+    // cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+
+    // mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah dirubah : " << dataku[1] << endl;
