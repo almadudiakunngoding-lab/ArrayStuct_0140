298bde831c940fa6029b8e50e9a070d5b439d578
almaudud <almadudiakunngoding@gmail.com>
2026-04-07 18:48:41 +0700

    menambahkan structdalamstruct

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -11,3 +11,24 @@ struct orang{
     detailAlamat alamat;
     int umur;
 };
+
+int main (){
+    //deklarasi variable struct
+    orang mhs;
+    //mengisi data
+    cout<<" isikan data nama : ";
+    cin>> mhs.nama;
+    cout<<"isikan data desa : ";
+    cin>> mhs.alamat.desa;
+    cout<<"isikan data kota : ";
+    cin>> mhs.alamat.kota;
+    cout<<"isikan data umur : ";
+    cin>> mhs.umur;
+    cout<<endl;
+    
+   cout << "Data Tampil" << endl;
+    cout << "nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
+}
